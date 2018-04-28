bf3d73b608eab8edc58a74648952944602950b7f
jgeorge247 <justingeorge247@gmail.com>
2018-04-28 04:37:28 -0400

    second commit

--- Capstone1_PigLat/Program.cs
+++ Capstone1_PigLat/Program.cs
@@ -10,50 +10,63 @@ namespace Capstone1_PigLat
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Pig Latin Translator!");
+            string UserChoice = "";
 
-            Console.WriteLine("Enter a word to translate: ");
-            string input = Console.ReadLine().ToLower();
-
-            string vowels = "aeiou";
-            int index = -1;
-
-            for (int i = 0; i < input.Length; i++)
+            do
             {
-                for (int j = 0; j < vowels.Length; j++)
+                Console.WriteLine("Welcome to the Pig Latin Translator!");
+
+                Console.WriteLine("Enter a word to translate: ");
+                string input = Console.ReadLine().ToLower();
+
+                string vowels = "aeiou";
+                int index = -1;
+
+                for (int i = 0; i < input.Length; i++)
                 {
-                    if (index < 0)
+                    for (int j = 0; j < vowels.Length; j++)
                     {
-                        if (input[i] == vowels[j])
+                        if (index < 0)
                         {
-                            //seeing if each of the letters are a vowel
-                            index = i;
-                           
+                            if (input[i] == vowels[j])
+                            {
+                                //seeing if each of the letters are a vowel
+                                index = i;
+                            }
                         }
                     }
                 }
-            }
-
-            if (index > 0)
-            {
-
-                Console.WriteLine(index);
-                string end = input.Substring(index);
-
-                string Beginning = input.Substring(0, index);
-
-                string pigLat = (end + Beginning + "ay");
-
-                Console.WriteLine($"Your word translated is {pigLat}");
-            }
-            else
-            {
-                if (index == 0)
+                if (index > 0)
                 {
-                    //if the word starts with a vowel, index is 0 and this happens
-                    Console.WriteLine($"Your new word is {input}way");
+                    Console.WriteLine(index);
+                    string end = input.Substring(index);
+
+                    string Beginning = input.Substring(0, index);
+
+                    string pigLat = (end + Beginning + "ay");
+
+                    Console.WriteLine($"Your word translated is {pigLat}");
                 }
-            }
+                else
+                {
+                    if (index == 0)
+                    {
+                        //if the word starts with a vowel, index is 0 and this happens
+                        Console.WriteLine($"Your new word is {input}way");
+                    }
+                }
+                do
+                {
+                    Console.WriteLine("Would you like to translate another word? (Y/N)");
+
+                    UserChoice = Console.ReadLine().ToUpper();
+                    if (UserChoice != "Y" && UserChoice != "N")
+                    {
+                        Console.WriteLine("Invalid input. Please write (Y/N)");
+                    }
+                } while (UserChoice != "Y" && UserChoice != "N");
+
+            } while (UserChoice == "Y"); 
         }
     }
 }
